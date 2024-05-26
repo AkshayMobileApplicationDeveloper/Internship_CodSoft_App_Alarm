f6fc79c30cad7405cf82446ff520556550306669
AkshayMobileApplicationDeveloper <akshaykumarprajapati@hotmail.com>
2024-05-26 13:40:44 +0530

    how to input data and show the logcat

--- app/src/main/java/com/alarmclock/MainActivity.java
+++ app/src/main/java/com/alarmclock/MainActivity.java
@@ -2,13 +2,40 @@ package com.alarmclock;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    AlarmManager alarmManager;
+    EditText idEditTime;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        idEditTime = findViewById(R.id.idEditTime);
+        button =findViewById(R.id.Btn_id);
+
+        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        //alarmManager.set(AlarmManager.RTC_WAKEUP);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("TAG", "onClick: "+idEditTime.getText().toString()  );
+
+            }
+
+        });
+
+
     }
 }
